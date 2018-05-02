752d07ab61a40ea8baff3308d8a2c5fbdecd795a
Andrew Patterson <apat24@mail.sfsu.edu>
2018-05-01 20:09:37 -0700

    implemented socket.io and beginning styles

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var socket_io = require("socket.io"); 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var messageRouter = require('./routes/message');
 
 var app = express();
+
+// Socket.io 
+var io = socket_io(); 
+app.io = io; 
+
+io.on("connection", function(socket) {
+  console.log("a user has connected");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/message', messageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
